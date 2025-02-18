586a839233ded98686dc2b691e15a0aad10241d8
Aakash Sharma <akaxarma@gmail.com>
2025-02-18 11:41:21 +0530

    files updated

---
+++ src/app/api/users/[id]/route.tsx
@@ -0,0 +1,14 @@
+import { user } from "../../../../util/db";
+
+import { NextResponse } from "next/server";
+
+export async function GET(request, {params}){
+    //console.log('hi');
+    //console.log(params);
+    const { id } = await params; 
+    const data = await user;  
+    const userData = data.filter((item)=>item.id==id)
+    return NextResponse.json(
+        userData.length==0?{result:"No result found",success:false}:{result:userData,success:true}, 
+        {status: 200})  
+}    
---
+++ src/util/db.js
@@ -0,0 +1,26 @@
+export const user = [
+    {
+        id: 1,
+        name: 'Anil',
+        age: 28,
+        email: 'anil@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Vikash',
+        age: 32,
+        email: 'vikash@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Shaun',
+        age: 25,
+        email: 'shaun@yahoo.com'
+    },
+    {
+        id: 4,
+        name: 'Amit',
+        age: 35,
+        email: 'amit@yahoo.com'
+    }
+]
