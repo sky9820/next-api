f16e2ee29dc54c1cbd21739788b3b428c314cbf2
Aakash Sharma <akaxarma@gmail.com>
2025-02-18 18:26:24 +0530

    post api created and tested

--- src/app/api/users/route.tsx
+++ src/app/api/users/route.tsx
@@ -1,7 +1,27 @@
 import { user } from "../../../util/db";
 import { NextResponse } from "next/server";
 
-export function GET(){
+export function GET() {
     const data = user;
-    return NextResponse.json(data, {status: 200})
+    return NextResponse.json(data, { status: 200 });
 }
+
+export async function POST( request ) {
+    const payload = await request.json();
+    
+    if(!payload.name || !payload.age || !payload.email){
+        return NextResponse.json({ result: "Required field not found", success: false}, {status: 400});
+    }
+    return NextResponse.json({ result: "Data Inserted", success: true },{status:201});
+}
+
+// export async function POST(request: Request) {
+//     try {
+//         const payload = await request.json(); // Ensure the request body is valid JSON
+//         console.log(payload);
+
+//         return NextResponse.json({ result: "hello ji" });
+//     } catch (error) {
+//         return NextResponse.json({ error: "Invalid JSON format" }, { status: 400 });
+//     }
+// }
